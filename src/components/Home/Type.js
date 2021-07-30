f4a347469f300e63c6416d7f7268d475230eba31
George Baker <46370151+xFlqx@users.noreply.github.com>
2021-07-30 06:40:03 +0100

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Gamers",
+          "Streamers",
+          "Friends",
+          "Innovators",
         ],
         autoStart: true,
         loop: true,
