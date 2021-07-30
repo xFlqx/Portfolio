f8ccda18bb86e61f51f6a0de31456f635213c4f9
George Baker <46370151+xFlqx@users.noreply.github.com>
2021-07-30 06:09:57 +0100

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,30 +8,29 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
+            Website was developed by <span className="purple">Flux </span>
             from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            <br /> The Nines Society is a collection of gamers set on making a difference through charity and education; as well as changing the scope of what a "clan" is capable of.
+            <br /> We offer our members the opportunity to enrich themselves with charitable outreach, accredited certificate courses, and a support system to help them reach their goals.
             <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from gaming, some other activities that we love to do are!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Streaming
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Educating
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Donating
             </li>
           </ul>
 
           <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "We can not solve our problems with the same thinking we use to create them."{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Woods</footer>
         </blockquote>
       </Card.Body>
     </Card>
